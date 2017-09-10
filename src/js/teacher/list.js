3468c68a02ee2f9478952d1c333d00b35a02ce19
zsq <597720334@qq.com>
2017-09-10 15:16:59 +0800

    左侧导航功能实现 首页与讲师列表页

--- src/js/teacher/list.js
+++ src/js/teacher/list.js
@@ -1,1 +1,3 @@
 var a = require('../common/a.js');
+var a = require('../common/header.js');
+var a = require('../common/aside.js');
--- src/js/user/login.js
+++ src/js/user/login.js
@@ -1,11 +1,20 @@
 // 当用户点击登陆按钮的时候，这个插件ajaxForm方法会自动监听submit事件
 // 然后阻止浏览器默认的刷新提交，然后自动变成ajax的方式发送请求。
+/* 服务端会返回用户信息 需要使用在导航 要用localStorage存储
+不用变量  因为登录成功后 页面会调到新的页面 每个页面的变量是独立的 */
+
+
+
+
+
+
 $('#login-form').ajaxForm({
     success: function(data) {
-        if(data.code == 200) {
+        if (data.code == 200) {
             alert('登陆成功');
+            localStorage.setItem('userinfo', JSON.stringify(data.result));
             location.href = '/dist';
-        }else {
+        } else {
             alert('登陆失败');
         }
     },
@@ -13,8 +22,6 @@ $('#login-form').ajaxForm({
         alert('登陆失败');
     }
 });
-
-
 // $('#login-form').on('submit', function(e) {
 
 //     $.ajax({
